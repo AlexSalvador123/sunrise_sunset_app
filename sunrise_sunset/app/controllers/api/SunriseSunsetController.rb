fa3f4c659b9b11a246c161f1232fefc1369938d8
apsalvador <apsalvador@fc.ul.pt>
2025-02-16 23:12:42 +0000

    controller changes - fetch_sunrise_sunset_data

--- sunrise_sunset/app/controllers/api/SunriseSunsetController.rb
+++ sunrise_sunset/app/controllers/api/SunriseSunsetController.rb
@@ -17,52 +17,6 @@ class Api::SunriseSunsetController < ApplicationController
     end
     
     private
-  
-    def fetch_sunrise_sunset_data1(location, start_date, end_date)
-      existing_data = DailySunriseSunset.where(location: location, date: start_date..end_date)
-      
-      coordinates = get_coordinates(location)
-
-      if coordinates[:error]
-        return { error: coordinates[:error] }
-      end
-      
-      locationLat = coordinates[:lat]
-      locationLng = coordinates[:lng]
-
-      if existing_data.any?
-        return existing_data
-      end
-      
-      url = "https://api.sunrisesunset.io/json?lat=#{locationLat}&lng=#{locationLng}&date_start=#{start_date}&date_end=#{end_date}"
-      response = HTTParty.get(url)
-
-      if response.success?
-        data = response.parsed_response["results"]
-
-        start_date_toDate = Date.parse(start_date)
-        end_date_toDate = Date.parse(end_date)
-
-        number_of_days = (end_date_toDate - start_date_toDate).to_i + 1
-
-        for elem in data do
-          sunrise_time = Time.parse(elem["sunrise"]).to_s
-          sunset_time = Time.parse(elem["sunset"]).to_s
-          golden_hour_time = Time.parse(elem["golden_hour"]).to_s
-
-
-          DailySunriseSunset.create(
-            location: location,
-            date: elem["date"],
-            sunrise: sunrise_time,
-            sunset: sunset_time,
-            golden_hour: golden_hour_time
-          )
-        end
-      else
-        { error: 'Failed to fetch data' }
-      end
-    end
 
     def fetch_sunrise_sunset_data(location, start_date, end_date)
       coordinates = get_coordinates(location)
@@ -71,44 +25,38 @@ class Api::SunriseSunsetController < ApplicationController
       location_lat = coordinates[:lat]
       location_lng = coordinates[:lng]
     
-      # Convert dates to an array of all days in the range
+      final_list = []
       missing_dates = (Date.parse(start_date)..Date.parse(end_date)).to_a
     
-      # Check which dates are already in the database
-      existing_records = DailySunriseSunset.where(location: location, date: missing_dates).pluck(:date)
-      missing_dates -= existing_records  # Remove dates that already exist
+      missing_dates.each do |date|
+        existing_record = DailySunriseSunset.find_by(location: location, date: date)
+        
+        if existing_record
+          final_list << existing_record
+        else
+          api_url = "https://api.sunrisesunset.io/json?lat=#{location_lat}&lng=#{location_lng}&date_start=#{date}&date_end=#{date}"
+          response = HTTParty.get(api_url)
     
-      # If all dates exist, return the stored data
-      if missing_dates.empty?
-        return DailySunriseSunset.where(location: location, date: start_date..end_date)
-      end
+          if response.success?
+            data = response.parsed_response["results"]
     
-      # Fetch only missing dates from API
-      missing_dates_str = missing_dates.map(&:to_s).join(",") # Format for API request
-      api_url = "https://api.sunrisesunset.io/json?lat=#{location_lat}&lng=#{location_lng}&date_start=#{start_date}&date_end=#{end_date}"
-      response = HTTParty.get(api_url)
-    
-      if response.success?
-        data = response.parsed_response["results"]
-    
-        # Store fetched data in DB
-        data.each do |elem|
-          DailySunriseSunset.create!(
-            location: location,
-            date: elem["date"],
-            sunrise: elem["sunrise"],
-            sunset: elem["sunset"],
-            golden_hour: elem["golden_hour"]
-          )
+            record = DailySunriseSunset.create!(
+              location: location,
+              date: data[0]["date"],
+              sunrise: data[0]["sunrise"],
+              sunset: data[0]["sunset"],
+              golden_hour: data[0]["golden_hour"]
+            )
+            
+            final_list << record
+          else
+            return { error: 'Failed to fetch data from API' }
+          end
         end
-      else
-        return { error: 'Failed to fetch data from API' }
       end
     
-      # Return the full dataset (both from DB and newly fetched)
-      DailySunriseSunset.where(location: location, date: start_date..end_date)
+      final_list
     end
-    
 
     def get_coordinates(location)
       api_key = ENV['GEO_API_KEY']
